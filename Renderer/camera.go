517084cb04e7ba8f3e4a8dcb58de28d8fed749f7
Joshua Wales <joshua.wales@gmail.com>
2015-09-08 16:56:45 +1000

    Implemented Camera setOrientation

--- Renderer/camera.go
+++ Renderer/camera.go
@@ -6,25 +6,28 @@ import(
 //A camera is an Entity
 type Camera struct {
 	renderer *Renderer
-	lookat, up vectorMath.Vector3
+	Translation, Lookat vectorMath.Vector3
+	Up vectorMath.Vector3
 }
 
 func CreateCamera(renderer *Renderer) Camera {
-	camera := Camera{
+	return Camera{
 		renderer: renderer,
-		lookat: vectorMath.Vector3{0,0,0},
-		up: vectorMath.Vector3{0,1,0},
+		Translation: vectorMath.Vector3{1,0,0},
+		Lookat: vectorMath.Vector3{0,0,0},
+		Up: vectorMath.Vector3{0,1,0},
 	}
-	return 
 }
 
 func (c *Camera) SetScale( scale vectorMath.Vector3 ) {} //na
 
 func (c *Camera) SetTranslation( translation vectorMath.Vector3 ) {
-	cameraLocation := vectorMath.Vector3{5*cosine,3*sine,5*sine}
-	glRenderer.Camera( translation, c.lookat, c.up )
+	c.Translation = translation
+	c.renderer.Camera( c.Translation, c.lookat, c.Up )
 }
 
 func (c *Camera) SetOrientation( orientation vectorMath.Quaternion  ) {
-	//TODO
+	direction := orientation.Apply( vectorMath.Vector3{1,0,0} )
+	c.Lookat = c.Translation.Add(direction)
+	c.renderer.Camera( c.Translation, c.lookat, c.Up )
 }
