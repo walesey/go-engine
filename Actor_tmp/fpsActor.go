5c9685823a9a32e3d998cc93c721494268042b2d
Joshua Wales <joshua.wales@gmail.com>
2015-09-14 17:41:12 +1000

    Renamed packages to temp names

---
+++ Actor_tmp/fpsActor.go
@@ -0,0 +1,42 @@
+package actor
+
+import (
+    "github.com/Walesey/goEngine/renderer"
+)
+
+//an actor that can move around bound by physics (gravity), can jump, can walk/run
+type FPSActor struct {
+	entity renderer.Entity
+}
+
+func (cam *FPSActor) StartMovingUp() {
+
+}
+
+func (cam *FPSActor) StartMovingDown() {
+
+}
+
+func (cam *FPSActor) StartMovingLeft() {
+
+}
+
+func (cam *FPSActor) StartMovingRight() {
+
+}
+
+func (cam *FPSActor) StopMovingUp() {
+
+}
+
+func (cam *FPSActor) StopMovingDown() {
+
+}
+
+func (cam *FPSActor) StopMovingLeft() {
+
+}
+
+func (cam *FPSActor) StopMovingRight() {
+
+}
