3ccc73ef9c989efc97c481791491837676099f31
Joshua <joshua.wales@gmail.com>
2016-09-15 21:24:03 +1000

    Fixed Tests

--- glfwController/controller_test.go
+++ glfwController/controller_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-gl/glfw/v3.1/glfw"
 	"github.com/stretchr/testify/assert"
+	"github.com/walesey/go-engine/controller"
 )
 
 type TestObject struct {
@@ -30,14 +31,13 @@ func TestActionMap(t *testing.T) {
 
 	to := TestObject{false}
 	c := NewActionMap()
-	c.BindAction(to.testAction, glfw.KeyW, glfw.Press)
-	c.BindAction(to.otherTestAction, glfw.KeyE, glfw.Release)
-	manager.AddController(c)
+	c.BindKeyAction(to.testAction, controller.KeyW, controller.Press)
+	c.BindKeyAction(to.otherTestAction, controller.KeyE, controller.Release)
+	manager.AddController(c.(Controller))
 
 	fmt.Println("About to trigger custom actions")
 	manager.KeyCallback(nil, glfw.KeyW, 0, glfw.Press, 0)
 	assert.True(t, to.testState, "test state not triggered by key binding")
 	manager.KeyCallback(nil, glfw.KeyE, 0, glfw.Release, 0)
 	assert.False(t, to.testState, "test state not triggered by key binding")
-
 }
--- util/mathUtils_test.go
+++ util/mathUtils_test.go
@@ -1,69 +1,69 @@
 package util
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/go-gl/mathgl/mgl32"
 	"github.com/stretchr/testify/assert"
 )
 
+func expectApproxEqual2D(t *testing.T, v1, v2 mgl32.Vec2) {
+	assert.True(t, v1.ApproxEqualThreshold(v2, 0.001), fmt.Sprintf("Expected %v to approx equal %v", v1, v2))
+}
+
 func TestRound(t *testing.T) {
-	n := 1.999
+	var n float32 = 1.999
 	i := Round(n, .5, 0)
 	assert.EqualValues(t, 2.0, i, "LengthSquared")
 }
 
 func TestRoundHalfUp(t *testing.T) {
-	n := 1.501
+	var n float32 = 1.501
 	i := RoundHalfUp(n)
 	assert.EqualValues(t, 2, i, "LengthSquared")
 }
 
-func TestApproxEqual(t *testing.T) {
-	assert.True(t, ApproxEqual(1.53121, 1.5, 0.1), "ApproxEqual")
-	assert.False(t, ApproxEqual(1.53121, 1.5, 0.01), "ApproxEqual")
-	assert.True(t, ApproxEqual(1.49121, 1.5, 0.01), "ApproxEqual")
-}
-
 func TestPointToLineDist(t *testing.T) {
-	assert.EqualValues(t, 1, PointToLineDist(Vector3{0, 0, 0}, Vector3{1, 0, 1}, Vector3{0, 1, 0}))
+	assert.EqualValues(t, 1, PointToLineDist(mgl32.Vec3{0, 0, 0}, mgl32.Vec3{1, 0, 1}, mgl32.Vec3{0, 1, 0}))
 }
 
 func TestTwoSegmentIntersect_intersection(t *testing.T) {
-	intersect, err := TwoSegmentIntersect(Vector2{0, 0}, Vector2{2, 2}, Vector2{-1, 1}, Vector2{2, 1})
+	intersect, err := TwoSegmentIntersect(mgl32.Vec2{0, 0}, mgl32.Vec2{2, 2}, mgl32.Vec2{-1, 1}, mgl32.Vec2{2, 1})
 	assert.Nil(t, err, "TwoSegmentIntersect error should be nil")
-	assert.EqualValues(t, Vector2{1, 1}, intersect, "TwoSegmentIntersect intersect")
+	assert.EqualValues(t, mgl32.Vec2{1, 1}, intersect, "TwoSegmentIntersect intersect")
 }
 
 func TestTwoSegmentIntersect_noIntersection(t *testing.T) {
-	_, err := TwoSegmentIntersect(Vector2{0, 0}, Vector2{-2, -2}, Vector2{-1, 1}, Vector2{2, 1})
+	_, err := TwoSegmentIntersect(mgl32.Vec2{0, 0}, mgl32.Vec2{-2, -2}, mgl32.Vec2{-1, 1}, mgl32.Vec2{2, 1})
 	assert.NotNil(t, err, "TwoSegmentIntersect error should not be nil")
 }
 
 func TestTwoSegmentIntersect_largeNumbers(t *testing.T) {
-	intersect, err := TwoSegmentIntersect(Vector2{455, 494}, Vector2{974, 76}, Vector2{835, 174}, Vector2{961, 211})
+	intersect, err := TwoSegmentIntersect(mgl32.Vec2{455, 494}, mgl32.Vec2{974, 76}, mgl32.Vec2{835, 174}, mgl32.Vec2{961, 211})
 	assert.Nil(t, err, "TwoSegmentIntersect error should be nil")
-	expectApproxEqual2D(t, Vector2{847.693, 177.727}, intersect)
+	expectApproxEqual2D(t, mgl32.Vec2{847.693, 177.727}, intersect)
 }
 
 func TestSegmentCircleIntersect_intersectionEntry(t *testing.T) {
-	intersect, err := SegmentCircleIntersect(0.5, Vector2{2, 1}, Vector2{1, 3}, Vector2{2, 1})
+	intersect, err := SegmentCircleIntersect(0.5, mgl32.Vec2{2, 1}, mgl32.Vec2{1, 3}, mgl32.Vec2{2, 1})
 	assert.Nil(t, err, "SegmentCircleIntersect error should be nil")
-	expectApproxEqual2D(t, Vector2{1.776, 1.447}, intersect)
+	expectApproxEqual2D(t, mgl32.Vec2{1.776, 1.447}, intersect)
 }
 
 func TestSegmentCircleIntersect_intersectionExit(t *testing.T) {
-	intersect, err := SegmentCircleIntersect(0.5, Vector2{2, 1}, Vector2{2, 1}, Vector2{1, 3})
+	intersect, err := SegmentCircleIntersect(0.5, mgl32.Vec2{2, 1}, mgl32.Vec2{2, 1}, mgl32.Vec2{1, 3})
 	assert.Nil(t, err, "SegmentCircleIntersect error should be nil")
-	expectApproxEqual2D(t, Vector2{1.776, 1.447}, intersect)
+	expectApproxEqual2D(t, mgl32.Vec2{1.776, 1.447}, intersect)
 }
 
 func TestSegmentCircleIntersect_2intersections(t *testing.T) {
-	intersect, err := SegmentCircleIntersect(0.5, Vector2{2, 1}, Vector2{1, 3}, Vector2{4, -2})
+	intersect, err := SegmentCircleIntersect(0.5, mgl32.Vec2{2, 1}, mgl32.Vec2{1, 3}, mgl32.Vec2{4, -2})
 	assert.Nil(t, err, "SegmentCircleIntersect error should be nil")
-	expectApproxEqual2D(t, Vector2{1.905, 1.491}, intersect)
+	expectApproxEqual2D(t, mgl32.Vec2{1.905, 1.491}, intersect)
 }
 
 func TestSegmentCircleIntersect_noIntersection(t *testing.T) {
-	_, err := SegmentCircleIntersect(0.5, Vector2{2, 1}, Vector2{1, 1}, Vector2{5, -2})
+	_, err := SegmentCircleIntersect(0.5, mgl32.Vec2{2, 1}, mgl32.Vec2{1, 1}, mgl32.Vec2{5, -2})
 	assert.NotNil(t, err, "SegmentCircleIntersect error should not be nil")
 }
